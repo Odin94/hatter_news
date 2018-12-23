f0ed64166743e7dc12019cb776d9b1726c0bc48d
Odin <kammerloher.andreas@gmail.com>
2018-12-23 22:00:09 +0100

    Add basic layout

---
+++ lib/news_post.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NewsPost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Container(
+            color: Colors.white,
+            height: 300,
+            child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                  bottomLeft: Radius.circular(2.0),
+                  bottomRight: Radius.circular(2.0),
+                )),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset(
+                        'images/test.png',
+                        height: 160.0,
+                        fit: BoxFit.cover,
+                      ),
+                      Text("Test")
+                    ]))));
+  }
+}
